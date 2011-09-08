01238fbcf1742f7a80cc50283714cfb93a58cf98
frost-nzcr4 <frost-nzcr4@gmail>
2011-09-08 22:53:01 +0400

    [Wmxi] upd test

--- tests/webmoney/WmxiTest.php
+++ tests/webmoney/WmxiTest.php
@@ -1,19 +1,29 @@
 <?php
 class WmxiTest extends PHPUnit_Framework_TestCase {
 	protected $Wmxi;
+	protected $authn;
 
 	protected function setUp() {
+		$this->authn = WebmoneyAuthn::getAuthn();
 		$this->Wmxi = new WMXI();
-		$this->Wmxi->Light(array('key' => '12345678901', 'cer' => '12345678901', 'pass' => '12345678901'));
+		$this->Wmxi->Light($this->authn['light']);
 	}
 
-	public function testInvalidInterfaceX8() {
+	public function testX8InvalidArgs() {
 		$wmid  = '';
 		$purse = '';
 
 		$result = $this->Wmxi->X8($wmid, $purse);
-		$this->assertInstanceOf('WMXIResult', $result);
-		$this->assertEquals(58, $result->ErrorCode());
+		$this->assertEquals(0, $result->ErrorCode());
+	}
+
+	public function testX8ValidWmid() {
+	  $wmid  = $this->authn['publicWmids']['WmAttestationCenter'];
+	  $purse = '';
+
+	  // Search Webmoney Attestation Center.
+	  $result = $this->Wmxi->X8($wmid, $purse);
+	  $this->assertEquals(1, $result->ErrorCode());
 	}
 }
 ?>
