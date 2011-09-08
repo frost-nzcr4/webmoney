b9f7e845c7f83ada12dc04d5420ca8d37cc342c4
frost-nzcr4 <frost-nzcr4@gmail>
2011-09-08 22:54:35 +0400

    [Webmoney] upd test

--- tests/webmoney/WebmoneyTest.php
+++ tests/webmoney/WebmoneyTest.php
@@ -5,28 +5,58 @@ class WebmoneyTest extends PHPUnit_Framework_TestCase {
 
 	protected function setUp() {
 		$this->authn = WebmoneyAuthn::getAuthn();
-		$this->Webmoney = new Webmoney();
+		$this->Webmoney = new Webmoney($this->authn['cert']);
 		$this->Webmoney->Light($this->authn['light']);
+	}
+
+	public static function InvalidDataProvider() {
+		$wmid    = $this->authn['wmid'];
+		$purseId = $this->authn['purse'];
+		$srcPurse = new Purse($wmid, $purseId);
+
+		$wmid    = $this->authn['wmid'];
+		$purseId = $this->authn['purse'];
+		$dstPurse = new Purse($wmid, $purseId);
+
+		return array(
+	  	array(
+				$srcPurse,
+				$dstPurse,
+				0,
+				0.001,
+				0)
+	  );
+	}
+
+	/**
+	 * @dataProvider InvalidDataProvider
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testInvalidData($srcPurse, $dstPurse, $tranid, $amount, $period, $pcode, $desc, $wminvid, $onlyauth) {
+	  $this->Webmoney->transferFunds($srcPurse, $dstPurse, $tranid, $amount, $period, $pcode, $desc, $wminvid, $onlyauth);
 	}
 
 	public function testTransferFunds() {
 		$wmid    = $this->authn['wmid'];
 		$purseId = $this->authn['purse'];
-		$sourcePurse = new Purse($wmid, $purseId);
+		$srcPurse = new Purse($wmid, $purseId);
 
 		// Search yourself by your WMID & Purse ID.
-		$result = $this->Webmoney->X8($wmid, $purseId);
-
-		$wmid  = $this->authn['publicWmids']['WmAttestationCenter'];
-		//$purse = $this->authn['purse'];
-
-		// Search Webmoney Attestation Center.
-		$result = $this->Webmoney->X8($wmid);
-
+		$result = $this->Webmoney->X8($srcPurse->getWmid(), $srcPurse->getId());
 		$this->assertEquals(1, $result->ErrorCode());
-		$this->assertEquals(58, $result);
 
-		$result = $this->Webmoney->transferFunds($wmid, $purseId);
+		$wmid    = $this->authn['wmid'];
+		$purseId = $this->authn['purse'];
+		$dstPurse = new Purse($wmid, $purseId);
+
+		// Search destination purse by WMID & Purse ID.
+		$result = $this->Webmoney->X8($dstPurse->getWmid(), $dstPurse->getId());
+		$this->assertEquals(1, $result->ErrorCode());
+		$this->assertEquals(123, (int) $result->toObject()->testwmpurse->wmid->attributes()->available);
+		$this->assertEquals(123, (int) $result->toObject()->testwmpurse->wmid->attributes()->themselfcorrstate);
+		$this->assertEquals(1, $result);
+
+		$result = $this->Webmoney->transferFunds($srcPurse, $dstPurse, 0.001);
 		$this->assertEquals(999, $result);
 	}
 }
